d54ba9ce0a8115ec5fb44fa6ae431e3cd8936549
Vlad Nozhkin <mega.nozhkin@mail.ru>
2022-01-13 14:01:55 +0300

    renamed the Layar component to Layer

--- src/components/Profile/Layer/Layer.jsx
+++ src/components/Profile/Layer/Layer.jsx
@@ -1,10 +1,10 @@
-import style from './Layar.module.scss';
+import style from './Layer.module.scss';
 
-const Layar = () => {
-  const { layar } = style;
+const Layer = () => {
+  const { Layer } = style;
 
   return (
-    <section className={layar}>
+    <section className={Layer}>
       <img
         src="https://www.arcturians.ca/wp-content/uploads/2018/12/2-the-earth-from-space-brightness-in-night_1200x224-300dpi.jpg"
         alt="layer"
@@ -13,4 +13,4 @@ const Layar = () => {
   );
 };
 
-export default Layar;
+export default Layer;
